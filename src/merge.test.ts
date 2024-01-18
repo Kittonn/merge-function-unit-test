e0fb135f97f2bb6c4ea56d130dd4f3573d398bff
Kittonn <tonkittipod@gmail.com>
2024-01-18 22:24:25 +0700

    feat: merge function with unit test

--- src/merge.test.ts
+++ src/merge.test.ts
@@ -1,5 +1,55 @@
-import { hello } from "./merge";
+import { merge } from "./merge";
 
-test("hello", () => {
-  expect(hello("world")).toBe("Hello world");
+describe("merge", () => {
+  test("merges two sorted arrays", () => {
+    expect(merge([1, 5], [0, 4])).toEqual([0, 1, 4, 5]);
+  });
+
+  test("merges two sorted arrays of different lengths", () => {
+    expect(merge([1, 5, 7], [0, 4])).toEqual([0, 1, 4, 5, 7]);
+  });
+
+  test("merges two sorted arrays of different lengths", () => {
+    expect(merge([1, 5], [0, 4, 7])).toEqual([0, 1, 4, 5, 7]);
+  });
+
+  test("merges two empty arrays", () => {
+    expect(merge([], [])).toEqual([]);
+  });
+
+  test("merges an empty array with a non-empty array", () => {
+    expect(merge([], [1, 2])).toEqual([1, 2]);
+  });
+
+  test("merges a non-empty array with an empty array", () => {
+    expect(merge([1, 2], [])).toEqual([1, 2]);
+  });
+
+  test("merges two arrays with the same elements", () => {
+    expect(merge([1, 2, 3], [1, 2, 3])).toEqual([1, 1, 2, 2, 3, 3]);
+  });
+
+  test("merges two arrays with the same elements", () => {
+    expect(merge([1, 1, 1], [1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
+  });
+
+  test("merges two arrays with negative numbers", () => {
+    expect(merge([-2, -1, 0], [-3, -1, 1])).toEqual([-3, -2, -1, -1, 0, 1]);
+  });
+
+  test("merges two arrays with negative numbers", () => {
+    expect(merge([-3, -1, 1], [-2, -1, 0])).toEqual([-3, -2, -1, -1, 0, 1]);
+  });
+
+  test("merges two arrays with negative numbers", () => {
+    expect(merge([-3, -2, -1], [-3, -2, -1])).toEqual([-3, -3, -2, -2, -1, -1]);
+  });
+
+  test("merges two arrays with negative numbers", () => {
+    expect(merge([-3, -2, -1], [-3, -2, -1])).toEqual([-3, -3, -2, -2, -1, -1]);
+  });
+
+  test("merges two arrays with positive and negative numbers", () => {
+    expect(merge([-3, -2, -1], [0, 1, 2])).toEqual([-3, -2, -1, 0, 1, 2]);
+  });
 });
--- src/merge.ts
+++ src/merge.ts
@@ -1,5 +1,35 @@
-export function hello(name: string) {
-  return `Hello ${name}`;
+export function merge(
+  collection_1: number[],
+  collection_2: number[]
+): number[] {
+  const result: number[] = [];
+  let collection_1_index = 0;
+  let collection_2_index = 0;
+
+  while (
+    collection_1_index < collection_1.length &&
+    collection_2_index < collection_2.length
+  ) {
+    if (collection_1[collection_1_index] < collection_2[collection_2_index]) {
+      result.push(collection_1[collection_1_index]);
+      collection_1_index++;
+    } else {
+      result.push(collection_2[collection_2_index]);
+      collection_2_index++;
+    }
+  }
+
+  while (collection_1_index < collection_1.length) {
+    result.push(collection_1[collection_1_index]);
+    collection_1_index++;
+  }
+
+  while (collection_2_index < collection_2.length) {
+    result.push(collection_2[collection_2_index]);
+    collection_2_index++;
+  }
+
+  return result;
 }
 
-console.log(hello("world"));
+console.log(merge([1, 5], [0, 4]));
